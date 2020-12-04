0f969f2f78fa7b6bbdd206b11d99ef8fe78167c7
Gabimaru <aashrader@yahoo.fr>
2020-12-04 11:28:55 +0100

    de toute baoutay

---
+++ src/components/CheckStatus.js
@@ -0,0 +1,24 @@
+/*import React from 'react'
+import {
+    Heading,
+    Input,
+    VStack,
+    HStack,
+    Button,
+    Center,
+    Text
+  } from '@chakra-ui/core'
+
+
+  const checkStatus = (x,y) => {
+    return <Text color={x}>{y}</Text>
+  }
+
+  const coloredGreen = "green.400"
+  const coloredRed = "red.400"
+
+  const connected = "connected"
+  const disconnect = "disconnect"
+
+  export default CheckStatus
+  */
---
+++ src/components/DarkMode.js
@@ -0,0 +1,14 @@
+/*
+import React from "react";
+import { ThemeProvider, ColorModeProvider } from "@chakra-ui/core";
+import customTheme from "./theme";
+
+function TurnOnColorMode({ children }) {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <ColorModeProvider>{children}</ColorModeProvider>
+    </ThemeProvider>
+  );
+}
+
+*/
